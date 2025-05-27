3acfdf36d2e4b0459cf1752fea9d5e930c2b7ff0
mklsh <mostafaklch@hotmail.com>
2025-05-27 15:26:40 +0300

    updates

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::post('/login', [AuthenticationController::class, 'login'])->middleware('w
 Route::post('/logout', [AuthenticationController::class, 'logout'])->middleware(['web', 'auth:sanctum']);
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login', [AuthenticationController::class, 'login'])->middleware('web');
+    Route::post('/login', [AuthenticationController::class, 'login'])->middleware('web')->name('auth.login');
     Route::post('/logout', [AuthenticationController::class, 'logout'])->middleware(['web', 'auth:sanctum']);
     Route::get('/me', [AuthenticationController::class, 'me'])->middleware(['web', 'auth:sanctum']);
 
