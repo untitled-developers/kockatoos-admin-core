6b28500b1eee66a87d5e1ecb3382bd229500dba9
mklsh <mostafaklch@hotmail.com>
2025-06-23 15:28:46 +0300

    dev mode

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,21 @@
 use UntitledDevelopers\KockatoosAdminCore\Http\Controllers\AdminUiDocsController;
 use UntitledDevelopers\KockatoosAdminCore\Skyforge\SkyForgeController;
 
-Route::middleware(['auth'])->group(function() {
+Route::middleware(['web'])->group(function() {
+    if (app()->environment('local', 'development')) {
+        Route::prefix('skyforge')->group(function () {
+            Route::get('/', [SkyForgeController::class, 'index'])
+                ->name('skyforge.index');
+            Route::get('/{table}', [SkyForgeController::class, 'table'])
+                ->name('skyforge.table-details');
+        });
 
-    Route::prefix('skyforge')->group(function () {
-        Route::get('/', [SkyForgeController::class, 'index'])
-            ->name('skyforge.index');
-        Route::get('/{table}', [SkyForgeController::class, 'table'])
-            ->name('skyforge.table-details');
-    });
-
-    Route::prefix('admin-ui')->group(function () {
-        Route::get('/', [AdminUiDocsController::class, 'index']);
-        Route::get('/assets/{path}', [AdminUiDocsController::class, 'assets'])->where('path', '.*');
-        Route::get('/{any}', [AdminUiDocsController::class, 'catchAll'])->where('any', '.*');
-    });
+        Route::prefix('admin-ui')->group(function () {
+            Route::get('/', [AdminUiDocsController::class, 'index']);
+            Route::get('/assets/{path}', [AdminUiDocsController::class, 'assets'])->where('path', '.*');
+            Route::get('/{any}', [AdminUiDocsController::class, 'catchAll'])->where('any', '.*');
+        });
+    }
 });
 
 
