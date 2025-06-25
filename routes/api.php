b9f24ddc56153b38cd058f0f941760fa9ebc1910
mklsh <mostafaklch@hotmail.com>
2025-06-25 13:43:36 +0300

    added api to auth routes

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use UntitledDevelopers\KockatoosAdminCore\Http\Controllers\AuthenticationControl
 use UntitledDevelopers\KockatoosAdminCore\Http\Controllers\BlobsController;
 use UntitledDevelopers\KockatoosAdminCore\Http\Controllers\RoutesController;
 
-Route::post('/login', [AuthenticationController::class, 'login'])->middleware('web')->name('auth.login');
-Route::post('/logout', [AuthenticationController::class, 'logout'])->middleware(['web', 'auth:sanctum']);
-Route::get('/me', [AuthenticationController::class, 'me'])->middleware(['web', 'auth:sanctum']);
+Route::post('api/login', [AuthenticationController::class, 'login'])->middleware('web')->name('auth.login');
+Route::post('api/logout', [AuthenticationController::class, 'logout'])->middleware(['web', 'auth:sanctum']);
+Route::get('api/me', [AuthenticationController::class, 'me'])->middleware(['web', 'auth:sanctum']);
 
 Route::middleware(['web', 'auth:sanctum'])->group(function () {
     Route::prefix('api')->group(function () {
