a1f8e48ed5c8f0528d58f4a2c9189d4ccca9382f
mklsh <mostafaklch@hotmail.com>
2025-10-20 14:23:50 +0300

    mfa

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,12 @@ use UntitledDevelopers\KockatoosAdminCore\Http\Controllers\BlobsController;
 use UntitledDevelopers\KockatoosAdminCore\Http\Controllers\MfaController;
 use UntitledDevelopers\KockatoosAdminCore\Http\Controllers\RoutesController;
 
-Route::post('api/login', [AuthenticationController::class, 'login'])->name('auth.login');
+Route::post('api/login', [AuthenticationController::class, 'login'])->middleware('web')->name('auth.login');
 Route::post('api/logout', [AuthenticationController::class, 'logout'])->middleware(['web', 'auth:sanctum']);
 Route::get('api/me', [AuthenticationController::class, 'me'])->middleware(['web', 'auth:sanctum']);
 
 // Public MFA routes
-Route::post('api/mfa/hasMfa', [MfaController::class, 'hasMfa'])->name('mfa.hasMfa');
+Route::post('api/mfa/hasMfa', [MfaController::class, 'hasMfa'])->middleware('web')->name('mfa.hasMfa');
 
 Route::middleware(['web', 'auth:sanctum'])->group(function () {
     Route::prefix('api')->group(function () {
