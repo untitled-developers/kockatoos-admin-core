e479fb8fdf8e1b1e4475e6fc544d0bf4aea9c1f3
mklsh <mostafaklch@hotmail.com>
2026-05-14 14:09:53 +0300

    blob model updates

--- src/Models/Blob.php
+++ src/Models/Blob.php
@@ -3,9 +3,30 @@
 namespace UntitledDevelopers\KockatoosAdminCore\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Blob extends BaseModel
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
+    protected $fillable = [
+        'url',
+        'type',
+        'size',
+        'ext',
+        'name',
+        'directory',
+        'sort_number',
+    ];
+
+    protected $casts = [
+        'size'        => 'integer',
+        'sort_number' => 'integer',
+    ];
+
+    public function getIsImageAttribute(): bool
+    {
+        return Str::startsWith($this->type, 'image/');
+    }
 }
